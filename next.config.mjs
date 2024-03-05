acca0b27b99dda60456e748404f133b166130210
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2024-03-05 17:22:49 +0200

    feat: initial commit

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./lib/env/client.ts");
+jiti("./lib/env/server.ts");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
